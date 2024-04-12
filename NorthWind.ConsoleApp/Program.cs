e1868df2af25f08e3858b73f50d6248b4728a37c
David-ImSorry <jd.barbetti@gmail.com>
2024-04-11 23:50:51 -0500

    Program Modificacinones (Principios y Host)

--- NorthWind.ConsoleApp/Program.cs
+++ NorthWind.ConsoleApp/Program.cs
@@ -1,7 +1,26 @@
-﻿using NorthWind.ConsoleApp.Services;
+﻿using Microsoft.Extensions.Hosting;
+using NorthWind.ConsoleApp.Services;
 using NorthWind.Entities.Interfaces;
+using NorthWind.Writers;
+using Microsoft.Extensions.DependencyInjection;
 
-IUserActionWriter Writer = default;
+HostApplicationBuilder Builder = Host.CreateApplicationBuilder();
+Builder.Services.AddSingleton<IUserActionWriter, DebugWriter>();
+Builder.Services.AddSingleton<AppLogger>();
+Builder.Services.AddSingleton<ProductService>();
+using var AppHost = Builder.Build();
+
+IUserActionWriter Writer = new FileWriter();
+
+IUserActionWriter Writer = new ConsoleWriter();
 
 AppLogger Logger = new AppLogger(Writer);
 Logger.WriteLog("Application Started.");
+ProductService Service = new ProductService(Writer);
+Service.Add("Demo", "Azúcar Refinada");
+/*     IMPLEMENTACIONES DE TRES PRINCIPIOS IMPORTANTES
+ * AppLogger y los Writers: Responsabilidad Única
+ * AppLogger: Abierto pero Cerrado
+ * AppLogger: Inversión de dependencias. Los módulos de alto nivel
+ * son independientes de los detalles de implementación
+ * */
